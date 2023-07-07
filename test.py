a4a4e42a7ffddd9afdf67ebe552c22287856e701
jeongdonggi <dkrhank45@gmail.com>
2023-07-07 16:23:57 +0900

    readme update1

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import exam
 
 #print(exam.gcd(14, 12))
-#print(exam.multiply_inverse(20))
+#print(exam.multiply_inverse(7))
 #print(exam.hill_num("weliveinaninsecureworld"))
 #print(exam.simple_gcd(14,12))
